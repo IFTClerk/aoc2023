f393c686d3e24550b7c1e309caf732b264c58d42
IFTClerk <54573631+IFTClerk@users.noreply.github.com>
2023-12-03 13:15:23 +0000

    Day 3 Part 2

--- d03/main.f90
+++ d03/main.f90
@@ -79,9 +79,10 @@ SUBROUTINE PART1()
      DO
         c = scm(ni, nj)
         IF (ISN(c)) THEN
+           ! Find where this number ends
            nf = LOOKAHEAD(scm(1:, nj), ni)
-           ! WRITE(6,*) nf
            IF (ANY(ISS(scm(ni-1:ni+nf+1,nj-1:nj+1)))) THEN
+              ! if any symbol around this number, read it
               b = ''
               DO nk=0,nf
                  b = TRIM(b) // scm(ni+nk, nj)
@@ -90,9 +91,11 @@ SUBROUTINE PART1()
               IF (ios.GT.0) THEN
                  WRITE(6,*) "Tried to read ", b, " but failed"
               END IF
-              WRITE(6,*) s
+              ! WRITE(6,*) s
+
               sum = sum + s
            END IF
+           ! skip the numbers we already did
            ni = ni + nf
         END IF
         ni = ni + 1
@@ -105,10 +108,93 @@ SUBROUTINE PART1()
   WRITE(6,*) "-----------------"
 END SUBROUTINE PART1
 
+INTEGER FUNCTION ADJN(adj)
+  LOGICAL, DIMENSION(3,3) :: adj
+  adjn = 0
+  ! numbers in line
+  adjn = adjn + COUNT(adj(:,2))
+  ! numbers above and below
+  IF (adj(2,1)) THEN
+     adjn = adjn + 1
+  ELSE
+     adjn = adjn + COUNT(adj(:,1))
+  END IF
+  IF (adj(2,3)) THEN
+     adjn = adjn + 1
+  ELSE
+     adjn = adjn + COUNT(adj(:,3))
+  END IF
+
+END FUNCTION ADJN
+
+SUBROUTINE PART2()
+  IMPLICIT NONE
+  CHARACTER, DIMENSION(:,:), ALLOCATABLE :: scm
+  LOGICAL, DIMENSION(3,3) :: adj
+  CHARACTER c
+  CHARACTER*64 b
+  INTEGER nr, nc
+  INTEGER ios, sum, t, s
+  INTEGER ni, nj, nn, nii, njj
+  INTEGER nf, nb, nk
+
+  sum = 0
+
+  CALL MAKESCHEM(scm, nr, nc)
+  scm = TRANSPOSE(scm)
+
+  DO nj=1,nr
+     DO ni=1,nc
+        c = scm(ni, nj)
+        IF (c.EQ.'*') THEN
+           adj = ISN(scm(ni-1:ni+1,nj-1:nj+1))
+           nn = ADJN(adj)
+           ! continue if not exactly 2 numbers
+           IF (nn.NE.2) CYCLE
+
+           ! now we can read those numbers
+           ! reduce the top and bottom rows
+           IF (adj(2,1)) adj(:,1) = (/ .FALSE., .TRUE., .FALSE. /)
+           IF (adj(2,3)) adj(:,3) = (/ .FALSE., .TRUE., .FALSE. /)
+
+           s = 1
+           DO njj=1,3
+              DO nii=1,3
+                 IF (adj(nii,njj)) THEN
+                    ! find the whole number
+                    nf = LOOKAHEAD(scm(ni+(nii-2):,nj+(njj-2)), 1)
+                    nb = LOOKAHEAD(scm(ni+(nii-2):0:-1,nj+(njj-2)), 1)
+
+                    ! read number
+                    b = ''
+                    DO nk=0,nf+nb
+                       b = TRIM(b) // scm((ni+nii-2)-nb+nk, nj+njj-2)
+                    END DO
+                    READ(b, *, IOSTAT=ios) t
+                    IF (ios.GT.0) THEN
+                       WRITE(6,*) "Tried to read ", TRIM(b), " but failed"
+                    END IF
+
+                    s = s * t
+                 END IF
+              END DO
+           END DO
+           WRITE(6,*) s
+           sum = sum + s
+        END IF
+     END DO
+  END DO
+
+  WRITE(6,*) "-----------------"
+  WRITE(6,*) "Part 2", sum
+  WRITE(6,*) "-----------------"
+END SUBROUTINE PART2
+
 END MODULE MOD
 
 PROGRAM MAIN
   USE MOD
 
-  CALL PART1()
+  ! CALL PART1()
+  CALL PART2()
 END PROGRAM MAIN
