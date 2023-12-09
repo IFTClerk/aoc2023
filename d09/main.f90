fcb9d7a3137a34e52f96dfd01dc0cedbb0a4c108
IFTClerk <yiwen.yang@outlook.com.au>
2023-12-09 06:15:53 +0000

    Day 9

---
+++ d09/main.f90
@@ -0,0 +1,100 @@
+MODULE MOD
+  USE UTILS
+  IMPLICIT NONE
+
+  ! CHARACTER*(*), PARAMETER :: fin = "test01.txt"
+  ! INTEGER, PARAMETER :: ns = 6
+  ! CHARACTER*(*), PARAMETER :: fin = "test02.txt"
+  CHARACTER*(*), PARAMETER :: fin = "input.txt"
+  ! CHARACTER*(*), PARAMETER :: fin = "inputjack.txt"
+  INTEGER, PARAMETER :: ns = 21
+CONTAINS
+
+SUBROUTINE EXTRAPOLATE(seq, finn, firn)
+  INTEGER, DIMENSION(ns) :: seq, finn, firn
+  INTEGER, DIMENSION(:), ALLOCATABLE :: diff, odiff
+  INTEGER i, j
+
+  finn = 0
+  finn(ns) = seq(ns)
+  firn = 0
+  firn(ns) = seq(1)
+  DO i=ns,1,-1
+     IF (ALLOCATED(odiff)) DEALLOCATE(odiff)
+     ALLOCATE(odiff(i))
+     IF (i.EQ.ns) THEN
+        odiff = seq
+     ELSE
+        odiff = diff
+     END IF
+
+     IF (ALLOCATED(diff)) DEALLOCATE(diff)
+     ALLOCATE(diff(i-1))
+
+     FORALL(j=1:i-1) diff(j) = odiff(j+1) - odiff(j)
+     IF ((i-1).GT.0) THEN
+        finn(i-1) = diff(i-1)
+        firn(i-1) = diff(1)
+     END IF
+     IF (ALL(diff.EQ.0)) EXIT
+  END DO
+END SUBROUTINE EXTRAPOLATE
+
+SUBROUTINE PART1()
+  IMPLICIT NONE
+  INTEGER, DIMENSION(ns) :: seq, finn, firn
+  INTEGER ios, s
+
+  s = 0
+  OPEN(10, FILE=fin, STATUS='OLD')
+
+  DO
+     READ(10, *, IOSTAT=ios) seq
+     IF (ios.EQ.IOSTAT_END) EXIT
+
+     CALL EXTRAPOLATE(seq, finn, firn)
+     s = s + SUM(finn)
+  END DO
+
+  CLOSE(10)
+
+  WRITE(6,*) "-----------------"
+  WRITE(6,*) "Part 1", s
+  WRITE(6,*) "-----------------"
+END SUBROUTINE PART1
+
+SUBROUTINE PART2
+  IMPLICIT NONE
+  INTEGER, DIMENSION(ns) :: seq, finn, firn, back
+  INTEGER ios, s, i
+
+  s = 0
+  OPEN(10, FILE=fin, STATUS='OLD')
+
+  DO
+     READ(10, *, IOSTAT=ios) seq
+     IF (ios.EQ.IOSTAT_END) EXIT
+
+     CALL EXTRAPOLATE(seq, finn, firn)
+     back = 0
+     DO i=1,ns-1
+        back(i+1) = firn(i+1) - back(i)
+     END DO
+     s = s + back(ns)
+  END DO
+
+  CLOSE(10)
+
+  WRITE(6,*) "-----------------"
+  WRITE(6,*) "Part 2", s
+  WRITE(6,*) "-----------------"
+END SUBROUTINE PART2
+
+END MODULE MOD
+
+PROGRAM MAIN
+  USE MOD
+
+  ! CALL PART1()
+  CALL PART2()
+END PROGRAM MAIN
