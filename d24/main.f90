b9206ec3654b472b06ed28dd53a4ac47904c00b9
IFTClerk <54573631+IFTClerk@users.noreply.github.com>
2023-12-24 14:31:34 +0000

    Day 24 but for real(128)

--- d24/main.f90
+++ d24/main.f90
@@ -9,6 +9,7 @@ MODULE MOD
   CHARACTER*(*), PARAMETER :: fin = "input.txt"
   REAL(KIND=REAL128), PARAMETER :: minp=200000000000000_real128, maxp=400000000000000_real128
 
+  INTEGER, PARAMETER :: INT128=SELECTED_INT_KIND(32)
 CONTAINS
 
 ELEMENTAL LOGICAL FUNCTION INAREA(val)
@@ -71,11 +72,87 @@ SUBROUTINE PART1()
   WRITE(6,*) "-----------------"
 END SUBROUTINE PART1
 
+RECURSIVE FUNCTION DET128(mat, n) RESULT(det)
+  INTEGER, INTENT(IN) :: n
+  INTEGER i, sgn
+  REAL(KIND=REAL128), DIMENSION(n,n), INTENT(IN) :: mat
+  REAL(KIND=REAL128), DIMENSION(n-1,n-1) :: minor
+  REAL(KIND=REAL128) det
+
+  IF (n.EQ.1) THEN
+     det = mat(1,1)
+  ELSE
+     det = 0
+     sgn = 1
+     DO i=1,n
+        minor(:,:(i-1)) = mat(2:, :i-1)
+        minor(:,i:) = mat(2:, i+1:)
+        det = det + sgn * mat(1,i) * DET128(minor, n-1)
+        sgn = -sgn
+     END DO
+  END IF
+END FUNCTION DET128
+
 SUBROUTINE PART2
   IMPLICIT NONE
+  CHARACTER l*256
+  INTEGER ios, ia, i, n
+  INTEGER(KIND=INT128) s
+  REAL(KIND=REAL128) det
+  REAL(KIND=REAL128), DIMENSION(6,6) :: A, Ai
+  REAL(KIND=REAL128), DIMENSION(6) :: B, sol
+  REAL(KIND=REAL128), DIMENSION(3,3) :: pos, vel
+
+  s = 0
+  OPEN(10, FILE=fin, STATUS='OLD')
+  i = 0
+  DO
+     i = i + 1
+     IF (i.GT.3) EXIT
+     READ(10, "(A)", IOSTAT=ios) l
+     IF (ios.EQ.IOSTAT_END) EXIT
+     ia = INDEX(l, "@")
+     l(ia:ia) = ','
+     READ(l, *) pos(:,i), vel(:,i)
+  END DO
+  CLOSE(10)
+
+  A = 0
+  B = 0
+
+  ! column 1
+  A(:,1) = [REAL(KIND=REAL128) :: (vel(2,i)-vel(2,1), 0, vel(3,1)-vel(3,i), i=2,3)]
+  ! column 2
+  A(:,2) = [REAL(KIND=REAL128) :: (vel(1,1)-vel(1,i), vel(3,i)-vel(3,1), 0, i=2,3)]
+  ! column 3
+  A(:,3) = [REAL(KIND=REAL128) :: (0, vel(2,1)-vel(2,i), vel(1,i)-vel(1,1), i=2,3)]
+  ! column 4
+  A(:,4) = [REAL(KIND=REAL128) :: (pos(2,1)-pos(2,i), 0, pos(3,i)-pos(3,1), i=2,3)]
+  ! column 5
+  A(:,5) = [REAL(KIND=REAL128) :: (pos(1,i)-pos(1,1), pos(3,1)-pos(3,i), 0, i=2,3)]
+  ! column 6
+  A(:,6) = [REAL(KIND=REAL128) :: (0, pos(2,i)-pos(2,1), pos(1,1)-pos(1,i), i=2,3)]
+  ! CALL PRINTMAT(A)
+  ! B is a single column vector
+  B(:) = [REAL(KIND=REAL128) :: (pos(2,1)*vel(1,1)-pos(1,1)*vel(2,1)+pos(1,i)*vel(2,i)-pos(2,i)*vel(1,i), &
+                                  pos(3,1)*vel(2,1)-pos(2,1)*vel(3,1)+pos(2,i)*vel(3,i)-pos(3,i)*vel(2,i), &
+                                  pos(1,1)*vel(3,1)-pos(3,1)*vel(1,1)+pos(3,i)*vel(1,i)-pos(1,i)*vel(3,i), &
+                                  i=2,3)]
+
+  ! Cramer's rule
+  n = 6
+  det = DET128(A,n)
+  DO i=1,3
+     Ai = A
+     Ai(:,i) = B(:)
+     sol(i) = DET128(Ai,n) / det
+  END DO
+
+  ! take nearest integer because precision issues
+  s = SUM(NINT(sol, KIND=INT128))
 
   WRITE(6,*) "-----------------"
-  WRITE(6,*) "Go use Mathematica"
+  WRITE(6,*) "Part 2", s
   WRITE(6,*) "-----------------"
 END SUBROUTINE PART2
 
@@ -84,6 +161,6 @@ END MODULE MOD
 PROGRAM MAIN
   USE MOD
 
-  CALL PART1()
-  ! CALL PART2()
+  ! CALL PART1()
+  CALL PART2()
 END PROGRAM MAIN
